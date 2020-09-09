5064313c92c0a0fea7e340d665919f2a719e3d17
jbalandran <javierbalandran@gmail.com>
2020-09-08 22:34:27 -0700

    new asp dotnet core web api back end project

---
+++ quiz-backend/quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        // POST api/questions
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
