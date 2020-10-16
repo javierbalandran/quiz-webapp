2aa94cebc968095dd4c477e4a324c109c3eedfdd
Javier Balandran <javierbalandran@gmail.com>
2020-10-16 11:22:02 -0700

    New Angular Routing and Navigation between posting and displaying questions

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,23 +3,36 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 import { ApiService } from './api.service';
 import { QuestionsComponent } from './questions.component';
+import { HomeComponent } from './home.component';
+import { NavComponent } from './nav.component';
+
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'questions', component: QuestionsComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +40,7 @@ import { QuestionsComponent } from './questions.component';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     MatToolbarModule,
     MatInputModule,
     MatCardModule,
