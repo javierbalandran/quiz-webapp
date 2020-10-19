912896321dc21d5ebcd547407321fede034210bf
Javier Balandran <javierbalandran@gmail.com>
2020-10-19 13:51:02 -0700

    Reactive forms with FormBuilder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -43,6 +43,7 @@ const routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
--- frontend/src/app/register.component.ts
+++ frontend/src/app/register.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from './api.service';
 
 @Component({
     templateUrl: './register.component.html'
 })
 export class RegisterComponent {
-    constructor(private api: ApiService) {}
+    form;
+    
+    constructor(private api: ApiService, private fb: FormBuilder) {
+        this.form = fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        })
+    }
 
-    ngOnInit() {
+    register() {
+        console.log(this.form);
     }
 }
